c49ba99559aa513e0428f4e96a16503046dfe875
AncientYoshi <paiswanpyae2002@gmail.com>
2024-11-27 14:28:03 +0630

    update and modify

--- javascript1/js/11x.js
+++ javascript1/js/11x.js
@@ -1,4 +1,4 @@
-const array1 = [
+const array1 = JSON.parse(localStorage.getItem("array1")) || [
   {
     name: "see",
     date: "2012-01-01",
@@ -12,19 +12,22 @@ const array1 = [
     date: "2022-2-23",
   },
 ];
+localStorage.setItem("array1", JSON.stringify(array1));
 rendorToDoList();
 function rendorToDoList() {
   let todoHTMLlist = "";
   for (let i = 0; i < array1.length; i++) {
     const todoHTML = array1[i];
     const html = `<div>${todoHTML.name}</div><div> ${todoHTML.date}</div>
-    <button class='delete-input' onclick='array1.splice(${i},1); rendorToDoList()'>
-    Delete
-    </button>
-    `;
+      <button class='delete-input' onclick='array1.splice(${i},1); rendorToDoList()'>
+      Delete
+      </button>
+      `;
     todoHTMLlist += html;
   }
   console.log(todoHTMLlist);
+  localStorage.setItem("array1", JSON.stringify(array1));
+
   document.querySelector(".show-rendor").innerHTML = todoHTMLlist;
 }
 
