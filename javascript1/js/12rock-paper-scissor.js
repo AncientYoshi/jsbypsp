238c7eec928ecd4981f112e5f624130611060101
AncientYoshi <paiswanpyae2002@gmail.com>
2024-11-29 10:19:55 +0630

    updated

---
+++ javascript1/js/12rock-paper-scissor.js
@@ -0,0 +1,102 @@
+let show = JSON.parse(localStorage.getItem("show")) || {
+  win: 0,
+  loss: 0,
+  tie: 0,
+};
+updateScoreElement();
+updateResult();
+let autoPlaying = false;
+let intervalId;
+function autoPlay() {
+  if (!autoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = computeNo();
+      calculateResult(playerMove);
+    }, 1000);
+    autoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    autoPlaying = false;
+  }
+}
+
+console.log(JSON.parse(localStorage.getItem("show")));
+function computeNo() {
+  const radomNo = Math.random();
+  let computerNo = "";
+  if (radomNo >= 0 && radomNo <= 1 / 3) {
+    computerNo = "rock";
+  } else if (radomNo >= 1 / 3 && radomNo <= 2 / 3) {
+    computerNo = "paper";
+  } else if (radomNo >= 2 / 3 && radomNo <= 1) {
+    computerNo = "scissor";
+  }
+  updateResult2(computerNo);
+  return computerNo;
+}
+//const computerNo = computeNo();
+function calculateResult(cal) {
+  let result = "";
+  const computerNo = computeNo();
+
+  if (cal === "scissor") {
+    if (computerNo === "rock") {
+      result = "You lose";
+    } else if (computerNo === "paper") {
+      result = "You win";
+    } else if (computerNo === "scissor") {
+      result = "Tie";
+    }
+  } else if (cal === "paper") {
+    if (computerNo === "rock") {
+      result = "You win";
+    } else if (computerNo === "paper") {
+      result = "Tie";
+    } else if (computerNo === "scissor") {
+      result = "You lose";
+    }
+    //console.log(result);
+  } else if (cal === "rock") {
+    if (computerNo === "rock") {
+      result = "Tie";
+    } else if (computerNo === "paper") {
+      result = "You lose";
+    } else if (computerNo === "scissor") {
+      result = "You win";
+    }
+  }
+  if (result === "You win") {
+    show.win += 1;
+  } else if (result === "You lose") {
+    show.loss += 1;
+  } else {
+    show.tie += 1;
+  }
+  localStorage.setItem("show", JSON.stringify(show));
+  updateScoreElement();
+  console.log(result);
+  updatemove(result);
+  updateResult(cal);
+  //alert(`You chose ${cal} and Computer is ${computerNo} and ${result}
+  //Win ${show.win}, Tie ${show.tie}, Loss ${show.loss}`);
+}
+function updateScoreElement() {
+  document.querySelector(
+    ".js-btn"
+  ).innerHTML = `Win ${show.win}, Tie ${show.tie}, Loss ${show.loss}`;
+}
+function updateResult(cal) {
+  document.querySelector(
+    ".js-btn-result"
+  ).innerHTML = `      You chose <img src="images/${cal}-emoji.png" alt="" class="move you-pick" />
+`;
+}
+function updatemove(result) {
+  document.querySelector(".js-btn-move").innerHTML = result;
+}
+function updateResult2(result) {
+  document.querySelector(
+    ".js-btn-result2"
+  ).innerHTML = `      Computer chose <img src="images/${result}-emoji.png" alt="" class="move" />
+`;
+}
---
+++ javascript1/js/12to-do-list.js
@@ -0,0 +1,51 @@
+const array1 = [
+  {
+    name: "see",
+    date: "2012-01-01",
+  },
+  {
+    name: "look at window",
+    date: "2012-5-01",
+  },
+  {
+    name: "handsome",
+    date: "2022-2-23",
+  },
+];
+rendorToDoList();
+function rendorToDoList() {
+  let todoHTMLlist = "";
+  array1.forEach(function (todoHTML, i) {
+    const html = `<div>${todoHTML.name}</div><div> ${todoHTML.date}</div>
+    <button class='delete-input' onclick='array1.splice(${i},1); rendorToDoList()'>
+    Delete
+    </button>
+    `;
+    todoHTMLlist += html;
+  });
+  /*for (let i = 0; i < array1.length; i++) {
+    const todoHTML = array1[i];
+    const html = `<div>${todoHTML.name}</div><div> ${todoHTML.date}</div>
+    <button class='delete-input' onclick='array1.splice(${i},1); rendorToDoList()'>
+    Delete
+    </button>
+    `;
+    todoHTMLlist += html;
+  }*/
+  console.log(todoHTMLlist);
+  document.querySelector(".show-rendor").innerHTML = todoHTMLlist;
+}
+
+function getToDoinput() {
+  const name = document.querySelector(".get-input");
+  const inputElement = name.value;
+  const date = document.querySelector(".get-date");
+  const dateElement = date.value;
+  array1.push({
+    name: inputElement,
+    date: dateElement,
+  });
+  console.log(array1);
+  name.value = "";
+  rendorToDoList();
+}
